38a71da02718b609731ce94b432dc2f76ffb0938
keyelluna <lunakimlouren.a@gmail.com>
2026-03-06 07:33:32 +0800

    Refactor server.js for improved CORS options

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const routes = require('./routes/index.js')
 const allowedOrigins = [
   'http://127.0.0.1:5500',
   'https://productdrill.netlify.app'
-];
+], 
+optionsSuccessStatus: 200
 
 const corsOptions = {
   origin: function (origin, callback) {
@@ -44,6 +45,7 @@ app.use('/api', routes)
 app.listen(process.env.PORT, () => {
     console.log(`Server is runnimg on Port ${process.env.PORT}`)
 })
+
 
 
 
