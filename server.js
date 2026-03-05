e66ae8860bd13e37efea68738d2c6fa9877d8202
keyelluna <lunakimlouren.a@gmail.com>
2026-03-06 07:26:50 +0800

    Fix CORS allowed origins URL

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const routes = require('./routes/index.js')
 // 1. Define your allowed origins
 const allowedOrigins = [
   'http://127.0.0.1:5500',
-  'productdrill.netlify.app'
+  'https://productdrill.netlify.app'
 ];
 
 const corsOptions = {
@@ -44,5 +44,6 @@ app.use('/api', routes)
 app.listen(process.env.PORT, () => {
     console.log(`Server is runnimg on Port ${process.env.PORT}`)
 })
+
 
 
